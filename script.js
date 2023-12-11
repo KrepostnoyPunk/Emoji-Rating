442a7c0f42c758a26a4746d194ed03ff308187d3
FREEMAN-LT\stepa <stepansidorov3234@gmail.com>
2023-12-11 11:52:32 +0300

    Simple emoji rating.Done.

---
+++ script.js
@@ -0,0 +1,25 @@
+const starsEl=document.querySelectorAll('.fa-star')
+const emojisEl=document.querySelectorAll('.fa-regular')
+const colorsArray=['red', 'orange', 'gray', 'lightgreen', 'green']
+
+updateRating(2)
+
+starsEl.forEach((starEl, index)=>{
+    starEl.addEventListener('click', ()=>{
+        updateRating(index)
+    })
+})
+
+function updateRating(index) {
+    starsEl.forEach((starEl, idx)=>{
+        if(idx<index+1) {
+            starEl.classList.add('active')
+        } else{
+            starEl.classList.remove('active')
+        }
+    })
+    emojisEl.forEach((emojiEl)=>{
+        emojiEl.style.transform=`translateX(-${index*50}px)`
+        emojiEl.style.color=colorsArray[index]
+    })
+}
